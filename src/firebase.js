75b10e6e7a51c6db39e8ccb426065e8a5612f415
David Pinos <davidspinosp@gmail.com>
2024-02-14 17:09:32 -0500

    adding anlytics

---
+++ src/firebase.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDB82sxraBLyH__N3pE92ZhcVVZ0htDae4",
+  authDomain: "hrbot-e8686.firebaseapp.com",
+  projectId: "hrbot-e8686",
+  storageBucket: "hrbot-e8686.appspot.com",
+  messagingSenderId: "862741743565",
+  appId: "1:862741743565:web:5eaa1f4351018d855ba684",
+  measurementId: "G-ZH7FXXEP7N"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const analytics = getAnalytics(app);
+analytics().setCurrentScreen(window.location.pathname) // sets `screen_name` parameter
+analytics().logEvent('screen_view')
+
+export { auth };
