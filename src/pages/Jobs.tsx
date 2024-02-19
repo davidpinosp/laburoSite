ad83a19507b700f0e3c97e6bd59871b573066d7a
David Pinos <davidspinosp@gmail.com>
2024-02-18 23:22:27 -0500

    Adding fitering functionality

--- src/pages/Jobs.tsx
+++ src/pages/Jobs.tsx
@@ -3,9 +3,21 @@ import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 import "../assets/styles/jobs.css";
 import JobPost from "../components/JobPost";
+import TuneIcon from "@mui/icons-material/Tune";
+import { Button } from "@mui/material";
+interface PostProps {
+  position: string;
+  company: string;
+  location: string;
+  salary?: string;
+  posted?: Date;
+}
 function Jobs() {
   const [jobsnumber, setJobsNumber] = useState(12345);
-  const jobPositions = [
+  const [searchQuery, setSearchQuery] = useState("");
+  const [locationQuery, setLocationQuery] = useState("");
+  const [filteredJobs, setFilteredJobs] = useState<PostProps[]>([]); // Provide an initial value as an empty array
+  const [jobPositions, setJobPositions] = useState<PostProps[]>([
     {
       position: "Analista de Servicio al Cliente",
       company: "Pydaco Cía Ltda",
@@ -27,11 +39,26 @@ function Jobs() {
       location: "Quito, Ecuador",
     },
     // Add more job positions as needed
-  ];
+  ]);
+
+  const handleMouseEnter = (e: any) => {
+    e.target.style.background = "";
+  };
+  const handleMouseLeave = (e: any) => {
+    e.target.style.background = "maroon";
+  };
 
   const fetchJobs = () => {
     // get the jobs from firebase
     // get length
+    const filtered = jobPositions.filter(
+      (jobs) => jobs.location === "Quito, Ecuador"
+    );
+    setJobPositions(filtered);
+  };
+
+  const searchJobs = (query: string) => {
+    // get query and filter from available jobs
   };
 
   return (
@@ -43,7 +70,7 @@ function Jobs() {
             <input
               type="text"
               className="search-pill-input"
-              placeholder="Buscar "
+              placeholder="Posición "
             />
           </div>
 
@@ -54,11 +81,20 @@ function Jobs() {
               placeholder="Ubicación"
             />
           </div>
-          <div className="bg-laburo-green search-btn">Boton Buscar</div>
+          <div
+            className="bg-laburo-green search-btn"
+            onClick={() => {
+              fetchJobs();
+            }}
+          >
+            Buscar
+          </div>
         </div>
-        <div className="flx space-btwn w100 mt-25">
+        <div className="flx space-btwn w100  filters-menu">
           <div>{jobsnumber} resultados</div>
-          <div className="order-dropdown">Ordenar </div>
+          <div className="order-dropdown">
+            <TuneIcon />
+          </div>
         </div>
         {/* positions here  */}
         <div className="w100 mb-25">
---
+++ src/utils/jobsUtils.tsx
@@ -0,0 +1,3 @@
+const getJobsData = () => {};
+
+export { getJobsData };
